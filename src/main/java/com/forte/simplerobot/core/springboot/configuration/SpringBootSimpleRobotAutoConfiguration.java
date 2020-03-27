bd3d1852fbb3cb6e53316ad4d9bdd83e47f3c45d
ForteScarlet <ForteScarlet@163.com>
2020-03-27 18:36:27 +0800

    :tada: Initial commit.

---
+++ src/main/java/com/forte/simplerobot/core/springboot/configuration/SpringBootSimpleRobotAutoConfiguration.java
@@ -0,0 +1,70 @@
+package com.forte.simplerobot.core.springboot.configuration;
+
+import com.forte.qqrobot.*;
+import com.forte.qqrobot.bot.BotManager;
+import com.forte.qqrobot.depend.DependCenter;
+import com.forte.qqrobot.sender.senderlist.SenderGetList;
+import com.forte.qqrobot.sender.senderlist.SenderSendList;
+import com.forte.qqrobot.sender.senderlist.SenderSetList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author <a href="https://github.com/ForteScarlet"> ForteScarlet </a>
+ */
+@Configuration
+public class SpringBootSimpleRobotAutoConfiguration {
+
+    /**
+     * 主要用来获取main方法中的args参数
+     */
+    @Autowired
+    private ApplicationArguments arguments;
+
+    @Bean
+    public <
+            CONFIG extends BaseConfiguration,
+            SEND extends SenderSendList,
+            SET extends SenderSetList,
+            GET extends SenderGetList,
+            CONTEXT extends SimpleRobotContext<SEND, SET, GET>
+            >
+    CONTEXT getSimpleRobotContext(BaseApplication<CONFIG, SEND, SET, GET, CONTEXT> baseApplication, Application<CONFIG> app) {
+        return baseApplication.runWithApplication(app, arguments.getSourceArgs());
+    }
+
+    /**
+     * 向Springboot注入字符串消息转化器
+     */
+    @Bean
+    public MsgParser getMsgParser(SimpleRobotContext context) {
+        return context.getMsgParser();
+    }
+
+    /**
+     * 向Springboot注入监听消息触发器
+     */
+    @Bean
+    public MsgProcessor getMsgProcessor(SimpleRobotContext context) {
+        return context.getMsgProcessor();
+    }
+
+    /**
+     * 向Springboot注入bot管理器
+     */
+    @Bean
+    public BotManager getBotManager(SimpleRobotContext context) {
+        return context.getBotManager();
+    }
+
+    /**
+     * 向Springboot注入simpleRobot的依赖中心
+     */
+    @Bean
+    public DependCenter getDependCenter(SimpleRobotContext context) {
+        return context.getDependCenter();
+    }
+
+}
